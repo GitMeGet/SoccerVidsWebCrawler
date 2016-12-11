530045c07deab0b8eedd9bc9d6877024776d17b5
GitMeGet <tanyupeng1994@gmail.com>
2016-12-11 16:57:55 +0800

    add test for downloading url

---
+++ test_url.py
@@ -0,0 +1,6 @@
+from __future__ import unicode_literals
+from url import Url
+
+url = Url('http://www.timesoccer.com/video/hull-city-vs-crystal-palace-live-streaming-highlights.html')
+
+url.youtube_dl()
