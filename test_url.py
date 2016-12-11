97a85db4b7d2590ba62c33abdbbe43d41a092100
GitMeGet <tanyupeng1994@gmail.com>
2016-12-11 17:23:32 +0800

    Url: include video_path so that video is easily started

--- test_url.py
+++ test_url.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 from url import Url
+import os
 
-url = Url('http://www.timesoccer.com/video/hull-city-vs-crystal-palace-live-streaming-highlights.html')
+url = Url('http://www.timesoccer.com/video/burnley-vs-bournemouth-live-streaming-highlights.html')
 
 url.youtube_dl()
+
+os.startfile(url.video_path) 
--- url.py
+++ url.py
@@ -3,20 +3,25 @@ import re
 
 class Url:
 
+    common_video_path = 'C:/Users/Yu Peng/Desktop/LearnPythonTheHardWay/timesoccer/videos/'
+
     def __init__(self, url):
         self.status = 'none'
         self.url = url
+        self.video_path = ''
 
     def youtube_dl(self):
         ydl_opts = {
-            'outtmpl': 'C:/Users/Yu Peng/Desktop/LearnPythonTheHardWay/timesoccer/videos/%(title)s-%(id)s.%(ext)s'
+            'outtmpl': Url.common_video_path + '%(title)s.%(ext)s'
         }
         with YoutubeDL(ydl_opts) as ydl:
-            print('Downloading: ' + self.url)
             ydl.download([self.url])
+            info_dict = ydl.extract_info(self.url, download=False)
+            video_title = info_dict.get('title', None)
+            video_extension = info_dict.get('ext', None)
+            self.video_path = Url.common_video_path + video_title + '.' + video_extension
         status = 'downloaded'
-        print("Download done: " + self.url)
-
+        
     def parse_html(self):
         m = re.search(r'(\w+)-vs-(\w+)',self.url)
         
